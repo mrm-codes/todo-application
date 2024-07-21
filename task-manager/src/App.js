da7d56e101d1ed88a8c75ca24d4eb0f157f71f29
Mario Matundulo <mrmatundulo@gmail.com>
2024-07-21 06:32:32 -0500

    created user management folder and added register, login and profiles js files, plus i,ported axios for data management

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import ProjectManager from './Task Management/ProjectManager';
+import RegisterUser from './User Management/Create User/RegisterUser';
+import LoginUser from './User Management/Create User/Login';
 
 
 
 function App() {
   return (
     <div className="App">
-      <div className='User-Management'></div>
+      <div className='User-Management'>
+        <RegisterUser/>
+        <h3>Next</h3>
+        <LoginUser/>
+      </div>
       <div className='Task-Managment'></div>
     </div>
   );
